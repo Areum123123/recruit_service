e174d7c313f14869d16867d170567d0622c3e8c9
areum <kang10232@naver.com>
2024-07-23 15:58:45 +0900

    feat:스키마작성

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import { errorHandler } from './middlewares/error-handler.middleware.js';
+import { PORT_NUMBER } from './constant/env.constant.js';
+
+const app = express();
+const PORT = PORT_NUMBER;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  return res.status(200).send('서버가 실행중');
+});
+
+// app.use('/api', [UsersRouter, ResumesRouter]);
+
+app.use(errorHandler); //error미들웨어
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸습니다!');
+});
